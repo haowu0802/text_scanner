27ec864547da49ef01d9bc96ee8daa5cdf4fa4d7
Hao Wu <haowu0802@gmail.com>
2017-07-19 17:27:23 -0700

    remove res

--- res/common/defines/difficulty_easy.lua
+++
@@ -1,8 +0,0 @@
--- Define overrides when playing on Easy
-
--- Format for overwriting define values:
---
--- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
-
-NDefines.NAI.DIPLOMATIC_ACTION_GIFT_POWERBALANCE_FACTOR_PLAYER = 100
-NDefines.NAI.DIPLOMATIC_ACTION_OFFER_CONDOTTIERI_DISABLE_VERSUS_PLAYER_ENEMIES = 1
